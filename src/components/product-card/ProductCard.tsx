9b881a65d364853a6a5ba25c0c00099cca691fce
cascadetile <collent.credev@gmail.com>
2022-12-16 22:41:41 +0600

    fix: fix 'Remove item' button

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { IProduct } from '../products/products.slice';
-import { useAppDispatch } from '../../functions/store/store.hooks';
-import { addToCart, removeFromCart } from '../../pages/cart/cart.slice';
+import { useAppDispatch, useAppSelector } from '../../functions/store/store.hooks';
+import { addToCart, removeFromCart, getCartProducts } from '../../pages/cart/cart.slice';
 
 
 const ProductCard = (product: IProduct) => {
 
   const dispatch = useAppDispatch();
+  const cartProducts = useAppSelector(getCartProducts);
 
   const addToCartHandler = (product: IProduct) => dispatch(addToCart(product));
   const handleRemoveFromCart = (productId: number) => dispatch(removeFromCart(productId));
-
+  
   return (
     <div className='products__item' key={product.id}>
       <div className='products__item-discount'>-{product.discountPercentage}%</div>
@@ -19,9 +20,9 @@ const ProductCard = (product: IProduct) => {
       <div>${product.price}</div>
       <div>{product.rating.toFixed(1)}</div>
       <div className='products__item-btn-wrapper'>
-        <button className='products__item-btn' onClick={() => addToCartHandler(product)}>Add to card {product.amount}</button>
+        <button className='products__item-btn' onClick={() => addToCartHandler(product)}>Add to card</button>
+        <button disabled={cartProducts.find((item) => item.id === product.id) ? false : true} onClick={() => handleRemoveFromCart(product.id)}>Remove from cart</button>
         <button className='products__item-btn' onClick={() => addToCartHandler(product)}>Buy now</button>
-        {product.amount ? (<button onClick={() => handleRemoveFromCart(product.id)}>Remove from cart</button>) : ('')}
       </div>
     </div>
   )
