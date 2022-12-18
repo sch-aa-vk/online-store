3bc525722dc73173d4231bd60b2d49fcf25db853
Senchenko Amina <sch.aa.vk@gmail.com>
2022-12-18 18:50:15 +0600

    fix: add checkboxes for every brand

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,28 +8,35 @@ import { ProductCard } from 'components/productCard/ProductCard';
 import './home.css';
 import { useAppDispatch } from 'store/store.hooks';
 import { brandHandler } from 'store/slices/filters.slice';
+import { initialState } from 'store/database/products';
 
 export const Home = () => {
 
   const dispatch = useAppDispatch();
 
   const products = useSelector(getProductsSelector);
+  const brandList = Array.from(new Set(initialState.map(item => item.brand)));
+
   const brandSelect = (brand: {brand: string, checked: boolean}) => dispatch(brandHandler(brand));
 
   return(
     <div className="app">
       <Header />
       <AscendingSort/>
-       <h2 className='products__title'>Products</h2>
+      <div>
+        <h2>Brands</h2>
+        {brandList.map(brand => 
+          <label key={brand}>
+            <input key={brand} onClick={(e) => brandSelect({ brand: (e.target as HTMLInputElement).name, checked: (e.target as HTMLInputElement).checked })} type="checkbox" name={brand} id={brand.replace(" ", "")} />
+            {`${brand}  (${products.filter(product => product.brand === brand).length}/${initialState.filter(product => product.brand === brand).length})`}
+          </label>
+        )}
+      </div>
+      <h2 className='products__title'>Products</h2>
       <div className='products__item-wrapper'>
         {products.map(product =>
           <ProductCard key={product.id} amount={product.amount} id={product.id} title={product.title} description={product.description} price={product.price} discountPercentage={product.discountPercentage} rating={product.rating} stock={product.stock} brand={product.brand} category={product.category} thumbnail={product.thumbnail} images={product.images}></ProductCard>
         )}
-      </div>
-      <div>
-        <h2>Brands</h2>
-        {/* TODO: add hardcoded values for now, parse them from database object in the future */}
-        <input onClick={(e) => brandSelect( {brand: (e.target as HTMLInputElement).name, checked: (e.target as HTMLInputElement).checked} )} type="checkbox" name="Apple" id="Apple" /><label htmlFor="Apple">Apple</label>
       </div>
     </div>
   );
