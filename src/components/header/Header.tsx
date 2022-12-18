9535c231f8a73aceedc496f02a7060d5f94feecf
Senchenko Amina <sch.aa.vk@gmail.com>
2022-12-18 21:59:31 +0600

    feat: add products amount to the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import {Link} from 'react-router-dom';
 import { getTotalPrice } from 'store/slices/cart.slice';
+import { getProductsSelector } from 'store/slices/products.slice';
 import { useAppSelector } from 'store/store.hooks';
 
 import './header.css';
 
 export const Header = () => {
 
+  const products = useSelector(getProductsSelector);
   const totalPrice = useAppSelector(getTotalPrice);
 
   return(
     <header className='header'>
       <Link className='link' to={"/"}>Home</Link>
+      <h5>{products.length}</h5>
       <Link className='link' to={"/cart"}>Cart</Link>
       <h5>{totalPrice}</h5>
     </header>
