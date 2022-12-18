53e9d2ac4931f82b57558ba821de9dbf52ee789f
Senchenko Amina <sch.aa.vk@gmail.com>
2022-12-18 23:18:03 +0600

    feat: add amount of items in cart to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import {Link} from 'react-router-dom';
-import { getTotalPrice } from 'store/slices/cart.slice';
+import { getCartProducts, getTotalPrice } from 'store/slices/cart.slice';
 import { getProductsSelector } from 'store/slices/products.slice';
 import { useAppSelector } from 'store/store.hooks';
 
@@ -10,14 +10,18 @@ import './header.css';
 export const Header = () => {
 
   const products = useSelector(getProductsSelector);
+  const cartProducts = useAppSelector(getCartProducts);
   const totalPrice = useAppSelector(getTotalPrice);
+
+  const itemsInCart = cartProducts.map(item => item.amount).reduce((acc, curr) => acc + curr, 0);
 
   return(
     <header className='header'>
       <Link className='link' to={"/"}>Home</Link>
-      <h5>{products.length}</h5>
+      <h5>found: {products.length}</h5>
       <Link className='link' to={"/cart"}>Cart</Link>
       <h5>{totalPrice}</h5>
+      <h5>items in cart: {itemsInCart}</h5>
     </header>
   )
 }
