744c07b7281bb3890d805273a32b6e8dcdad5648
cascadetile <collent.credev@gmail.com>
2022-12-24 17:03:58 +0600

    feat: Buy Now button's logic

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { IProduct } from 'store/interface/IProduct'; 
 import { useAppDispatch, useAppSelector } from 'store/store.hooks';
 import { addToCart, removeFromCart, getCartProducts } from 'store/slices/cart.slice';
@@ -8,11 +8,28 @@ import './productCard.css';
 
 export const ProductCard = (product: IProduct) => {
 
+  const navigate = useNavigate();
+
   const dispatch = useAppDispatch();
   const cartProducts = useAppSelector(getCartProducts);
 
   const addToCartHandler = (product: IProduct) => dispatch(addToCart(product));
   const handleRemoveFromCart = (productId: number) => dispatch(removeFromCart(productId));
+  const buyNowHandler = (product: IProduct) => {
+    let productInCard = cartProducts.find((item) => item.id === product.id);
+
+    if (productInCard) {
+      if (productInCard.amount >= product.stock) {
+        navigate('/cart');
+      } else {
+        navigate('/cart');
+        return addToCartHandler(product);
+      }
+    } else {
+      navigate('/cart');
+      return addToCartHandler(product);
+    }
+  }
   
   return (
     <div className='products__item'>
@@ -26,7 +43,7 @@ export const ProductCard = (product: IProduct) => {
       <div className='products__item-btn-wrapper'>
         <button disabled={cartProducts.find((item) => item.amount >= product.stock) ? true : false} className='products__item-btn' onClick={() => addToCartHandler(product)}>Add to card</button>
         <button disabled={cartProducts.find((item) => item.id === product.id) ? false : true} onClick={() => handleRemoveFromCart(product.id)}>Remove from cart</button>
-        <button className='products__item-btn' onClick={() => addToCartHandler(product)}>Buy now</button>
+        <button className='products__item-btn' onClick={() => buyNowHandler(product)}>Buy now</button>
       </div>
     </div>
   )
