7711bcbc51013ad53e5a826928e849ca65126d9a
Senchenko Amina <sch.aa.vk@gmail.com>
2022-12-25 23:36:00 +0600

    feat: add breadcrumps and fix link to productPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
-import { initialState } from 'store/database/products';
 import store from 'store/store';
 
 import { Home } from 'pages/home/Home';
@@ -16,10 +14,8 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
-        {initialState.map(item => (
-          <Route key={item.id} path={`/products/${item.id}`} element={<ProductPage product={item}/>} />
-        ))}
-        <Route path='*' element={<Page404 />}/>
+        <Route path="/products/:productId" element={<ProductPage />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </Provider>
   );
